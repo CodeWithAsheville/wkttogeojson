c8800a677941a89ebadd54adc38613a8479a86c0
Patrick Conant <patrick@prcapps.com>
2015-03-19 20:48:13 -0400

    Stuff

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var parse = require('wellknown');
+var parseWKT = require('wellknown');
 
-
+var stringify = require('csv-stringify');
+var generate = require('csv-generate');
+var request = require('request');
+var parseCSV = require('csv-parse');
 
 
 var express = require('express');
@@ -22,14 +25,63 @@ app.get('/wkttogeojson', function(req, res){
 	  // res.send('id: ' + req.url);
 
 	  source_url = req.url.replace("/wkttogeojson?id=", "");
-	  res.send('Source URL: ' + source_url);
-	  
+	  var csv;
+	  var final_output;
+
+		request.get(source_url, function (error, response, body) {
+		    if (!error && response.statusCode == 200) {
+		        csv = body;
+		        // Continue with your processing here.
+
+
+				// require('should');
+
+				parseCSV(csv, {comment: '#'}, function(err, output){
+					// console.log("output", output);
+					for(index in output){
+						row = output[index];
+						console.log(row);
+						console.log(row[7]);
+						wkt_string = row[7];
+						geo_json= parseWKT(row[7]);
+						console.log(geo_json);
+
+						if(geo_json != null){
+
+							output[index][7] = JSON.stringify(geo_json);
+						}
+
+						// break;
+					}
+					final_output = output;
+
+
+					// input = [ [ '1', '2', '3', '4' ], [ 'a', 'b', 'c', 'd' ] ];
+					stringify(final_output, function(err, csv_output){
+					  // output.should.eql('1,2,3,4\na,b,c,d');
+		        	  res.send(csv_output);
+
+					});
+
+
+
+				  // output.should.eql([ [ '1', '2', '3', '4' ], [ 'a', 'b', 'c', 'd' ] ]);
+				});
+
+
+
+
+		    }
+		});
+
+
+
 //  res.sendFile(__dirname + '/index.html');
 });
 
 
 http.listen( (process.env.PORT || 3000), function(){
   console.log('listening on *:'+  (process.env.PORT || 3000) );
-  console.log(parse('POINT(1 2)'));
+  console.log(parseWKT('POINT(1 2)'));
 });
 
